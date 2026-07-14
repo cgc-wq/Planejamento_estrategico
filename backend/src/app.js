198948b9496dfe35d4edfc241dd2e7183f336feb
Thamires <thamiresdosanjosdasilva8.com>
2026-07-14 14:57:46 -0300

    uploads

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serving uploads directory
-app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // Rotas da API
 app.use('/api/auth', authRoutes);
